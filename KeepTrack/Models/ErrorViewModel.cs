6f30edb99690137643841895489d0ea963e3667b
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2020-05-25 09:36:42 +0100

    First sign up function now working
    People can now sign up and be logged in after they sign up. The password field verifies if the input has met certain criterion.
    Next up on the list for the signup page is email authentication and being able to login using Google or Facebook.

---
+++ KeepTrack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KeepTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
