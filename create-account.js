9e4b4362c17e18255959c24e522101f1e31d8f6c
Simeon Kenneth T <51843241+KenRelic@users.noreply.github.com>
2020-05-27 11:31:31 +0100

    Update create-account.js

--- create-account.js
+++ create-account.js
@@ -10,12 +10,15 @@ function validateForm() {
     let emailInput = createAccountForm['email'];
     let organisation = document.querySelector('.account-type');
 
-    if (passwordInput.value == "" && nameInput.value == "" && emailInput.value == "" && organisation.selectedIndex == 0) {
+    if (passwordInput.value == "" && nameInput.value == "" && emailInput.value == "") {
         passwordInput.parentElement.classList.add('failed');
         nameInput.parentElement.classList.add('failed');
         emailInput.parentElement.classList.add('failed');
         organisation.parentElement.classList.add('failed');
+        return false;
     }
+    console.log(emailInput.value, nameInput.value, passwordInput.value, selectedOption)
+    return createUser(emailInput.value, nameInput.value, passwordInput.value, selectedOption)
 
 }
 let togglePasswordIcon = document.getElementById('show-hide-password');
@@ -31,42 +34,103 @@ function showHidePassword() {
     }
 }
 
+
 let dropIcon = document.getElementById('drop-down-icon');
 let selectedOption = document.getElementById('select-input');
+let accountListLabel = document.getElementById('account-type-label');
+let targetSelectElem = document.querySelector('.select-account-type');
 dropIcon.addEventListener('click', toggleSelectDrop);
+targetSelectElem.addEventListener('click', toggleSelectDrop);
 selectedOption.addEventListener('click', toggleSelectDrop);
-
 function toggleSelectDrop() {
     let optionList = document.getElementById('select-account');
-    
-   if(optionList.style.display == 'none'){
-        optionList.style.display = 'block';
+    dropIcon.setAttribute('class', 'fas fa-sort-up');
+    switch (optionList.style.display) {
+        case 'block': optionList.style.display = 'none';
+            dropIcon.setAttribute('class', 'fas fa-sort-down');
+            break;
+        default: optionList.style.display = 'block';
+            dropIcon.setAttribute('class', 'fas fa-sort-up');
+            break;
+    }
+}
+
+let optionList = document.getElementById('select-account');
+let options = Array.from(optionList.children);
+window.onclick = function (event) {
+    let x = event.target;
+    if (x.parentElement.className === "form-group select-account-type") {
         dropIcon.setAttribute('class', 'fas fa-sort-up');
-   } else{
-       optionList.style.display = 'none';
-       dropIcon.setAttribute('class', 'fas fa-sort-down');
-   }
+        document.getElementById('select-account').style.display = "block";
+        if (x.id === "list-item1" || x.id === "list-item2") {
+            selectedOption.innerHTML = x.innerText;
+            document.getElementById('select-account').style.display = "none";
+            dropIcon.setAttribute('class', 'fas fa-sort-down');
+        }
+    } else {
+        document.getElementById('select-account').style.display = "none";
+        dropIcon.setAttribute('class', 'fas fa-sort-down');
+    }
 }
 
 
-let optionList = document.getElementById('select-account');
-optionList = Array.from(optionList.children);
-optionList.forEach(option => {
-    option.addEventListener('click',()=>{
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        accountListLabel.style.top = "0.5em";
+        accountListLabel.style.fontSize = "0.8em";
+        accountListLabel.style.left = "2.9em";
+        targetSelectElem.style.padding = "2em 0 1.5em 2.6em";
         selectedOption.innerHTML = option.innerText;
-        document.getElementById('select-account').style.display = 'none';
+        selectedOption.style.display = 'block';
+        optionList.style.display = "none";
         dropIcon.setAttribute('class', 'fas fa-sort-down');
+        if (selectedOption.innerText == "") {
+            accountListLabel.style.top = "1.5em";
+            accountListLabel.style.fontSize = "1em";
+            accountListLabel.style.left = "2.6em";
+            targetSelectElem.style.padding = "2.4em 0 2.1em 2.6em";
+        }
     })
 });
 
-
+//CHECK IF THERE IS CONTENT IN THE INPUT THEN PREVENT ANIMATION
+let inputBoxes = document.querySelectorAll('.form-input input');
+inputBoxes.forEach(box => {
+    box.addEventListener('focus', () => {
+        box.nextElementSibling.style.top = '0.4em';
+        box.nextElementSibling.style.left = '2.7em';
+    });
+    box.addEventListener('blur', () => {
+        if (box.value !== "") {
+            box.nextElementSibling.style.top = '0.4em';
+            box.nextElementSibling.style.left = '2.7em';
+        } else {
+            box.nextElementSibling.style.top = '2em';
+            box.nextElementSibling.style.left = '2.6em';
+        }
+    });
+    box.addEventListener('input', ()=>{
+        if(box.validity.patternMismatch || box.validity.typeMismatch){
+            box.parentElement.nextElementSibling.style.visibility = 'visible';
+            return false
+        }else{
+            box.parentElement.nextElementSibling.style.visibility = 'hidden';
+        }
+    })
+})
 //creat new user
-function createUser(fullName, email, password, accountType){
-    if(!localStorage.hasOwnProperty(email)){
-        localStorage.setItem(email,{'fullName':fullName,'password':password,'accountType':accountType})
-        
-    
-    }else{
+function createUser(fullName, email, password, accountType) {
+    if (!localStorage.hasOwnProperty(email)) {
+        localStorage.setItem(email, { 'fullName': fullName, 'password': password, 'accountType': accountType })
+        // Display confrimation page
+        document.getElementById('notification').style.display = "flex";
+    } else {
+        // show error that the username is already taken
+        //or check on INPUT.. 
 
     }
 }
+
+
+
+
