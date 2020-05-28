b5ea79c8a6e457efa6685622ce21d41c04865430
Simeon Kenneth T <51843241+KenRelic@users.noreply.github.com>
2020-05-28 22:32:07 +0100

    Update sign-in.js

--- sign-in.js
+++ sign-in.js
@@ -1,6 +1,7 @@
 // import { localStorage } from "./create-account.js";
 let localStorage = window.localStorage;
 let validationMessage = document.getElementById('validation-error');
+let loader = document.getElementById('loader-bg');
 
 
 function validateDetails() {
@@ -11,24 +12,29 @@ function validateDetails() {
     if (localStorage.hasOwnProperty(email.value)) {
         let userDetails = JSON.parse(localStorage[email.value])
         if (userDetails.password === password.value) {
-           validationMessage.innerHTML ='login in..'
+            validationMessage.innerHTML = 'login in...'
+            validationMessage.style.visibility = 'visible';
+            return setTimeout(() => { displayLoader() }, 1000);
         } else {
             //password incorrect
             validationMessage.innerHTML = '**Incorrect Password';
-            validationMessage.style.visibility = 'visible';
-        }
+            return validationMessage.style.visibility = 'visible';
+        };
     } else {
         //This email doesnt exist.
-        if (email.value == ""){
+        if (email.value == "") {
             validationMessage.innerHTML = "**The Email field is required";
-            validationMessage.style.visibility = 'visible';
+            return validationMessage.style.visibility = 'visible';
         } else {
-            validationMessage.innerHTML = "**This Email isn\'t registered yet";
-            validationMessage.style.visibility = 'visible';
-        }
-
-    }
-}
+            if (!email.checkValidity()) {
+                validationMessage.innerHTML = "**Please enter a valid email";
+            } else {
+                validationMessage.innerHTML = "**This Email isn\'t registered yet";
+            }
+            return validationMessage.style.visibility = 'visible';
+        };
+    };
+};
 
 let signInBtn = document.getElementById('sign-in-btn');
 signInBtn.addEventListener('click', validateDetails);
@@ -51,28 +57,65 @@ function showHidePassword() {
 let inputBoxes = document.querySelectorAll('.form-input input');
 inputBoxes.forEach(box => {
     box.addEventListener('focus', () => {
-        box.nextElementSibling.style.top = '0.5em';
-        box.nextElementSibling.style.left = '2.5em';
+        moveLabel(box, '0.5em', '2.5em', 'normal');
     });
-    box.addEventListener('blur', () => {        
+    box.addEventListener('blur', () => {
         if (box.value !== "") {
-            box.nextElementSibling.style.top = '0.5em';
-            box.nextElementSibling.style.left = '2.5em';
-            box.nextElementSibling.style.fontWeight = 'normal';
-            
-        //    if(localStorage.hasOwnProperty(inputBoxes[0].value)){
-        //     validationMessage.innerHTML = '**Email already exists';
-        //     validationMessage.style.visibility = 'visible';
-        //    }
-            
+            moveLabel(box, '0.5em', '2.5em', 'normal');
         } else {
-            box.nextElementSibling.style.top = '1.5em';
-            box.nextElementSibling.style.left = '2.5em';
-            box.nextElementSibling.style.fontWeight = '700';
+            moveLabel(box, '1.5em', '2.5em', '700');
         }
     });
 
 });
 
+function displayLoader() {
 
+    loader.style.display = "flex";
+    setTimeout(() => {
+        inputBoxes.forEach(box => {
+            box.value = '';
+            moveLabel(box, '1.5em', '2.5em', '700');
+        });
+        validationMessage.style.visibility = 'hidden';
+        loader.style.display = "none";
+    }, 5000);
+}
 
+function moveLabel(el, top, left, fontWeight) {
+    el.nextElementSibling.style.top = top;
+    el.nextElementSibling.style.left = left;
+    el.nextElementSibling.style.fontWeight = fontWeight;
+};
+
+//RETRIEVE PASSWORD
+let sendRetrievalRequestBtn = document.getElementById('retrieve-password-btn');
+let openPasswordRetrievalPageBtn = document.querySelector('.retrieve-password-link');
+let gotoSignInPageBtn = document.getElementById('to-signin-btn');
+let retrievalPage = document.getElementById('retrieve-password-page');
+let requestConfirmationPage = document.getElementById('retrieval-request-confirmation');
+
+openPasswordRetrievalPageBtn.addEventListener('click', displayPasswordRetrievalPage);
+sendRetrievalRequestBtn.addEventListener('click', sendPasswordRetrievalRequest);
+gotoSignInPageBtn.addEventListener('click', loadSignInPage)
+function displayPasswordRetrievalPage() {
+    loader.style.display = "flex";
+    setTimeout(() => {
+        retrievalPage.style.display = "flex";
+        loader.style.display = "none";
+    }, 2000);
+}
+
+sendRetrievalRequestBtn.addEventListener('click', sendPasswordRetrievalRequest);
+function sendPasswordRetrievalRequest() {
+    requestConfirmationPage.style.left = '0';
+}
+
+function loadSignInPage() {
+    loader.style.display = "flex";
+    retrievalPage.style.display = "none"
+    setTimeout(() => {
+        requestConfirmationPage.style.left = '101%';
+        loader.style.display = "none";
+    },2000)
+}
