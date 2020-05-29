699dbb6ba71d92af87a11ae612035cfe572014ab
Simeon Kenneth T <51843241+KenRelic@users.noreply.github.com>
2020-05-29 09:48:58 +0100

    Update create-account.js

--- create-account.js
+++ create-account.js
@@ -1,3 +1,13 @@
+//prevent scrolling
+function noScroll() {
+    document.body.style.height = '100vh';
+    document.body.style.overflow = 'hidden';
+}
+function allowScroll() {
+    document.body.style.height = 'unset';
+    document.body.style.overflow = 'unset';
+}
+
 //FORM VALIATION//
 let localStorage = window.localStorage;
 let validationError = document.getElementById('validation-error');
@@ -6,7 +16,6 @@ let dropIcon = document.getElementById('drop-down-icon');
 let selectedOption = document.getElementById('select-input');
 let accountListLabel = document.getElementById('account-type-label');
 let targetSelectElem = document.querySelector('.select-account-type');
-
 
 function validateForm() {
     // let forms = document.forms;
@@ -17,8 +26,9 @@ function validateForm() {
     let organisation = selectedOption.innerText;
 
     if (passwordInput.checkValidity() && nameInput.checkValidity() && emailInput.checkValidity() && organisation !== undefined) {
-        if (localStorage.hasOwnProperty([emailInput.value])) {
-
+        if (localStorage.hasOwnProperty(emailInput.value)) {
+            emailInput.parentElement.nextElementSibling.innerHTML = "**This email is already registered";
+            emailInput.parentElement.nextElementSibling.style.visibility = 'visible';
             return false;
         } else {
             createUser(emailInput.value, nameInput.value, passwordInput.value, selectedOption)
@@ -26,11 +36,9 @@ function validateForm() {
             setTimeout(() => {
                 // Display confrimation page
                 document.getElementById('notification').style.display = "flex";
+                return window.preventUserScroll() || noScroll();
             }, 300);
-            // return true;
         }
-
-
     } else {
         emailInput.checkValidity() ? '' : emailInput.parentElement.nextElementSibling.style.visibility = 'visible';
         passwordInput.checkValidity() ? '' : passwordInput.parentElement.nextElementSibling.style.visibility = 'visible';
@@ -38,8 +46,8 @@ function validateForm() {
         organisation !== "" ? '' : selectedOption.parentElement.nextElementSibling.style.visibility = 'visible';
         return false;
     }
-
 }
+
 let createAccountBtn = document.getElementById('create-account-btn');
 createAccountBtn.addEventListener('click', validateForm);
 let togglePasswordIcon = document.getElementById('show-hide-password');
@@ -48,10 +56,10 @@ function showHidePassword() {
     let passwordElem = document.getElementById('password');
     if (passwordElem.type == 'password') {
         passwordElem.type = 'text';
-        togglePasswordIcon.classList.value = "fas fa-eye-slash";
+        return togglePasswordIcon.classList.value = "fas fa-eye-slash";
     } else {
         passwordElem.type = 'password';
-        togglePasswordIcon.classList.value = "fas fa-eye";
+        return togglePasswordIcon.classList.value = "fas fa-eye";
     };
 };
 
@@ -83,56 +91,73 @@ window.onclick = function (event) {
         if (x.id === "list-item1" || x.id === "list-item2") {
             selectedOption.innerHTML = x.innerText;
             document.getElementById('select-account').style.display = "none";
-            dropIcon.setAttribute('class', 'fas fa-sort-down');
+            return dropIcon.setAttribute('class', 'fas fa-sort-down');
         }
     } else {
         document.getElementById('select-account').style.display = "none";
-        dropIcon.setAttribute('class', 'fas fa-sort-down');
+        return dropIcon.setAttribute('class', 'fas fa-sort-down');
     };
 };
 
 
 options.forEach(option => {
     option.addEventListener('click', () => {
-        selectedOption.parentElement.nextElementSibling.style.visibility = 'hidden';
-        accountListLabel.style.top = "0.5em";
-        accountListLabel.style.fontSize = "0.8em";
-        accountListLabel.style.left = "2.9em";
-        targetSelectElem.style.padding = "2em 0 1.5em 2.6em";
+        moveAccountListLabel(accountListLabel, "0", "0.8em", "2.7em", targetSelectElem, "1em 1em 1em 2.2em", selectedOption, "hidden");
+        // selectedOption.parentElement.nextElementSibling.style.visibility = 'hidden';
+        // accountListLabel.style.top = "0em";
+        // accountListLabel.style.fontSize = "0.8em";
+        // accountListLabel.style.left = "2.6em";
+        // targetSelectElem.style.padding = "1em 1em 1em 2.2em";
         selectedOption.innerHTML = option.innerText;
         selectedOption.style.display = 'block';
         optionList.style.display = "none";
         dropIcon.setAttribute('class', 'fas fa-sort-down');
         if (selectedOption.innerText == "") {
-            accountListLabel.style.top = "1.5em";
-            accountListLabel.style.fontSize = "1em";
-            accountListLabel.style.left = "2.6em";
-            targetSelectElem.style.padding = "2.4em 0 2.1em 2.6em";
-            selectedOption.parentElement.nextElementSibling.style.visibility = 'visible';
+            moveAccountListLabel(accountListLabel, "1.5em", "1em", "2.6em", targetSelectElem, "1em 1em 2.6em 2.3em", selectedOption, "visible");
+            // accountListLabel.style.top = "1.5em";
+            // accountListLabel.style.fontSize = "1em";
+            // accountListLabel.style.left = "2.6em";
+            // targetSelectElem.style.padding = "1em 1em 2.6em 2.3em";
+            // selectedOption.parentElement.nextElementSibling.style.visibility = 'visible';
         };
     });
 });
 
+function moveAccountListLabel(label, labelTop, labelFontSize, labelLeft, labelParentElem, parentElPadding, selectedOption, visibility) {
+    label.style.top = labelTop;
+    label.style.fontSize = labelFontSize;
+    label.style.left = labelLeft;
+    labelParentElem.style.padding = parentElPadding;
+    selectedOption.parentElement.nextElementSibling.style.visibility = visibility;
+}
 //CHECK IF THERE IS CONTENT IN THE INPUT THEN PREVENT ANIMATION
 let inputBoxes = document.querySelectorAll('.form-input input');
 inputBoxes.forEach(box => {
     box.addEventListener('focus', () => {
-        box.nextElementSibling.style.top = '0.4em';
+        box.nextElementSibling.style.top = '-0.1em';
         box.nextElementSibling.style.left = '2.7em';
     });
     box.addEventListener('blur', () => {
         if (box.value !== "") {
-            box.nextElementSibling.style.top = '0.4em';
+            box.nextElementSibling.style.top = '-0.1em';
             box.nextElementSibling.style.left = '2.7em';
-            // box.parentElement.nextElementSibling.style.visibility = 'hidden';
-             if (box.id == "email") {
-                box.checkValidity()? localStorage.hasOwnProperty(box.value) ?
-                box.parentElement.nextElementSibling.innerHTML = "**This email is already registered" : "" :"**Please fill a valid Email";
-                box.parentElement.nextElementSibling.style.visibility =  box.checkValidity()? 'hidden':'visible';
-            }
+            if (box.id == "email") {
+                if (box.checkValidity()) {
+                    if (localStorage.hasOwnProperty(box.value)) {
+                        box.parentElement.nextElementSibling.innerHTML = "**This email is already registered";
+                        box.parentElement.nextElementSibling.style.visibility = 'visible';
+                    } else {
+                        box.parentElement.nextElementSibling.style.visibility = 'hidden';
+                    };
+                } else {
+                    box.parentElement.nextElementSibling.innerHTML = "**Please fill a valid Email";
+                    box.parentElement.nextElementSibling.style.visibility = 'visible';
+                };
+            };
         } else {
-            box.nextElementSibling.style.top = '2em';
+            box.nextElementSibling.style.top = '1.5em';
             box.nextElementSibling.style.left = '2.6em';
+            box.parentElement.nextElementSibling.innerHTML = "**Please fill a valid Email";
             box.parentElement.nextElementSibling.style.visibility = 'visible';
         }
     });
